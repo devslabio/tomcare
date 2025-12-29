d956b622368ac29c79adf777197d67513fce7b3b
hubertit <hubert@itdevs.rw>
2025-12-29 15:05:11 +0200

    Increase header height from h-16 to h-20 and scale logo proportionally

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -38,7 +38,7 @@ export function Navigation() {
       {/* Main Navigation */}
       <nav className="bg-white border-b border-border">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-20">
           {/* Logo */}
           <Link href="/" className="flex items-center">
             <Image 
@@ -46,7 +46,7 @@ export function Navigation() {
               alt="TOMCARE Foundation" 
               width={200}
               height={40}
-              className="h-8 md:h-10 w-auto"
+              className="h-10 md:h-12 w-auto"
               priority
             />
           </Link>
